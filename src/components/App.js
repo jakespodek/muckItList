0172ee94e7d5000f29adeb60b448277c7464226f
Jake Spodek <jake.spodek1@gmail.com>
2021-05-17 18:49:45 -0400

    lotta stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,49 @@
+import { useEffect, useState } from 'react';
 import '../styles/App.css';
-import { Fragment } from 'react';
+import firebase from '../config/firebase';
+import RestaurantContainer from './RestaurantContainer.js'
 
 function App() {
+
+  const [ restaurants, setRestaurants ] = useState([]);
+  const [ userInput, setUserInput ] = useState('');
+
+  useEffect(() => {
+    const dbRef = firebase.database().ref();
+
+    dbRef.on('value', (response) => {
+      const newDataArray = []
+      const data = response.val();
+
+      for (let key in data) {
+        newDataArray.push({ key: key, name: data[key] });
+      }
+
+      setRestaurants(newDataArray);
+
+    });
+
+  }, []); // <------ dependancy array
+
   return (
-    <Fragment>
+    <div>
       <h1>Muck-it List</h1>
-      <h2>App goes here</h2>
-      <h3>Test</h3>
-    </Fragment>
+      <h2>Muck Definition</h2>
+      <RestaurantContainer restaurantList={restaurants}/>
+      <form action="submit">
+        <label htmlFor="input" className="sr-only">
+          Add a restaurant to your list
+        </label>
+        <input 
+          type="text"
+          id="input"
+          value={userInput}
+          onChange={handleUserInput} 
+        />
+        <button onClick={handleSubmitClick}>Add Restaurant</button>
+      </form>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/RestaurantContainer.js
@@ -0,0 +1,20 @@
+// RestaurantContainer.js
+const RestaurantContainer = ({ restaurantList, deleteRestaurant }) => {
+    return (
+        <ul className="restaurantContainer">
+            {
+                restaurantList.map((restaurant) => {
+                    return (
+                        // add key!!!
+                        <li className="restaurant" key={restaurant.key}>
+                            <p>Restaurant: {restaurant.name}</p>
+                            <button>Remove from list x</button>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+export default RestaurantContainer;
