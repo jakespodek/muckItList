e3ce21996cab159820639060d3499e684e9a9d26
Jake Spodek <jake.spodek1@gmail.com>
2021-05-19 13:02:17 -0400

    updated error handling

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,7 @@ function App() {
 
   const handleUserInput = (event) => {
     let inputValue = event.target.value;
-    inputValue.length < 50 ? setUserInput(inputValue) : alert('Please try a shorter restaurant name');
+    inputValue.length < 50 && inputValue.length > 0 ? setUserInput(inputValue) : alert('Please try a shorter restaurant name');
   };
 
   const handleRestaurantAdd = (event) => {
