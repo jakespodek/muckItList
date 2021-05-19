aa3da4b503756199e8b8e1d62da6bb39e162fe8e
Jake Spodek <jake.spodek1@gmail.com>
2021-05-19 18:04:40 -0400

    muck definition

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import '../styles/App.css';
 import firebase from '../config/firebase';
 import RestaurantContainer from './RestaurantContainer.js'
+import MuckDefinition from './MuckDefinition.js';
 
 function App() {
 
@@ -47,15 +48,15 @@ function App() {
 
   const handleRestaurantDelete = (restaurant) => {
     const dbRef = firebase.database().ref();
-    
+
     dbRef.child(restaurant.key).remove();
   };
 
   return (
     <div className="wrapper">
       <h1>Muck-it List</h1>
-      <h2>Make a list of restaurants you would like to go to</h2>
-      {/* <h2>Muck Definition</h2> */}
+      <h2>for the restaurants you want to check out before you check out</h2>
+      <MuckDefinition />
       <RestaurantContainer restaurantList={restaurants} deleteRestaurant={handleRestaurantDelete} />
       <form action="submit">
         <label htmlFor="restaurantName" className="sr-only">
@@ -68,6 +69,7 @@ function App() {
           onChange={ (event) => setRestaurantName(event.target.value) }
           minLength="0"
           maxLength="50"
+          placeholder="name of restaurant"
         />
         <label htmlFor="restaurantCity" className="sr-only">
           Specify the city, town, or area that the restaurant is in
@@ -79,6 +81,7 @@ function App() {
           onChange={ (event) => setRestaurantCity(event.target.value) }
           minLength="0"
           maxLength="50"
+          placeholder="location of restaurant (e.g. toronto)"
         />
         <button onClick={handleRestaurantAdd}>Add Restaurant</button>
       </form>
---
+++ src/components/MuckDefinition.js
@@ -0,0 +1,11 @@
+// MuckDefinition.js
+const MuckDefinition = () => {
+    return (
+        <div>
+            <h3>muck · /mək/ · <span>verb</span></h3>
+            <p>To eat voraciously; devour</p>
+        </div>
+    )
+}
+
+export default MuckDefinition;
