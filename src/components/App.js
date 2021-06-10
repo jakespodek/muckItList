c74c044595bef5b6d4db04139987f8f61b9ba7f9
Jake Spodek <jake.spodek1@gmail.com>
2021-06-10 15:51:37 -0400

    changed some stuff

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ function App() {
       const data = response.val();
 
       for (let key in data) {
-        newDataArray.push({ key: key, name: data[key].name, city: data[key].city });
+        newDataArray.unshift({ key: key, name: data[key].name, city: data[key].city });
       }
 
       setRestaurants(newDataArray);
@@ -42,7 +42,7 @@ function App() {
       </main>
       <footer>
         <h2>For the restaurants you need to check out before checking out</h2>
-        <p>Created by <a href="https://jakespodek.com/" target="_blank" rel="noopener noreferrer">Jake Spodek</a> at <a href="https://junocollege.com/" target="_blank" rel="noopener noreferrer">Juno College</a></p>
+        <p>Designed and developed by <a href="https://jakespodek.com/" target="_blank" rel="noopener noreferrer">Jake Spodek</a></p>
       </footer>
     </div>
   );
